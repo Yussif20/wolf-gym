6daedc1728e9f01b3110ad6da939f61e5f2e5eec
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-07-20 14:38:30 +0200

    making the navbar pt.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import Stack from "@mui/material/Stack";
+
+import Logo from "../assets/images/Logo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack>
+      <Link to="/">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <Stack>
+        <link></link>
+        <a href="#exercises">Exercises</a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
